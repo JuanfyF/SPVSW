a7c4b9a1070ab348755e0c22755a79fea0d8e9a3
JuanfyF <juanferf_17@hotmail.com>
2026-09-25 10:40:07 -0500

    ci: fix CI failures - install FUSE, fix after-pack executableName on Windows

--- apps/desktop/scripts/after-pack.js
+++ apps/desktop/scripts/after-pack.js
@@ -4,7 +4,11 @@ const path = require("path");
 module.exports = async function(context) {
   // afterPack: don't modify the binary, we'll patch AppRun later
   const { appOutDir } = context;
-  const binName = context.packager.executableName;
+  const binName = context.packager.executableName || context.packager.appInfo.productFilename;
+  if (!binName) {
+    console.log("afterPack: executableName not available, skipping");
+    return;
+  }
   const binPath = path.join(appOutDir, binName);
 
   if (!fs.existsSync(binPath)) {
